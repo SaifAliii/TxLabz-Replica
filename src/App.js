62e4651c4d1b296b8fed96a3ac26321c1b5def46
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-11-26 23:40:32 +0500

    Navbar in process

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Navbar from "./Components/Navbar/Navbar"
-import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,27 @@
-function Navbar(){
-    <nav className="nav-bar">
-        <a href="#">Portfolio</a>
-    </nav>
+import "../Navbar/Navbar.css";
+import logo from "../../logo.png";
+function Navbar() {
+  return (
+    <div className="nav-bar-container">
+      <a href="#" className="main-logo">
+        TXLABZ
+      </a>
+      <nav className="nav-bar">
+        <a href="#" className="nav-item">
+          Portfolio
+        </a>
+        <a href="#" className="nav-item">
+          Services
+        </a>
+        <a href="#" className="nav-item">
+          Testimonials
+        </a>
+        <a href="#" className="nav-item">
+          Cases
+        </a>
+      </nav>
+      <i class="dot fa-solid fa-circle"></i>
+    </div>
+  );
 }
 export default Navbar;
