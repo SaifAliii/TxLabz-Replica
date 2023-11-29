7d290468a452cfdcb32ed464a8b63b6bafe38c65
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-11-29 05:07:55 +0500

    Working on Portfolio Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { gsap } from "gsap";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import "./App.css";
+import Portfolio from "./Components/Portfolio/Portfolio";
 
 function App() {
   useEffect(() => {
@@ -33,13 +34,34 @@ function App() {
         });
       })
       .addTo(controller);
+
+    const scenePortfolio = new ScrollMagic.Scene({
+      triggerElement: ".portfolio-main",
+      triggerHook: 0.7, // Trigger when 50% of the element is in the viewport
+      reverse: true, // Don't reverse the animation when scrolling up
+    })
+      .on("leave", () => {
+        gsap.to(".portfolio-main", {
+          width: "95%",
+          borderRadius: "28px",
+        });
+      })
+      .on("enter", () => {
+        gsap.to(".portfolio-main", {
+          width: "100%",
+          borderRadius: 0,
+        });
+      })
+      .addTo(controller);
     // Optional: Add indicators for debugging
     scene.addIndicators();
+    scenePortfolio.addIndicators();
   }, []);
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Portfolio />
     </div>
   );
 }
---
+++ src/Components/Portfolio/Portfolio.js
@@ -0,0 +1,79 @@
+import "./Portfolio.css";
+import portfolioSample from "../../samplePortfolio.jpeg";
+function Portfolio() {
+  return (
+    <div className="portfolio">
+      <div className="portfolio-main">
+        <div className="portfolio-heading">
+          <h3>Featured Work</h3>
+          <p>Check out our projects, each of them equally aced.</p>
+        </div>
+        <div className="portfolio-projects-main">
+          <div className="portfolio-projects-row1">
+            <div className="portfolio-projects-sub">
+              <div className="portfolio-projects">
+                <a href="#">
+                  <img
+                    className="portfolio-image"
+                    src={portfolioSample}
+                    alt="portfolio image"
+                  />
+                </a>
+              </div>
+              <div className="portfolio-view-project">
+                View Project{" "}
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+            <div className="portfolio-projects-sub">
+              <div className="portfolio-projects">
+                <a href="#">
+                  <img
+                    className="portfolio-image"
+                    src={portfolioSample}
+                    alt="portfolio image"
+                  />
+                </a>
+              </div>
+              <div className="portfolio-view-project">
+                View Project{" "}
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+            {/* <div className="portfolio-projects-sub">
+              <div className="portfolio-projects">
+                <a href="#">
+                  <img
+                    className="portfolio-image"
+                    src={portfolioSample}
+                    alt="portfolio image"
+                  />
+                </a>
+              </div>
+              <div className="portfolio-view-project">
+                View Project{" "}
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div> */}
+            <div className="portfolio-projects-sub">
+              <div className="portfolio-projects">
+                <a href="#">
+                  <img
+                    className="portfolio-image"
+                    src={portfolioSample}
+                    alt="portfolio image"
+                  />
+                </a>
+              </div>
+              <div className="portfolio-view-project">
+                View Project{" "}
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Portfolio;
