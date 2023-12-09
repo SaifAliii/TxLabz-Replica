09c8f41a6294a6ae08d41ce488ab0cf99e3877e3
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-12-10 00:55:03 +0500

    Vision Section CSS Updated

--- src/Components/Vision/Vision.js
+++ src/Components/Vision/Vision.js
@@ -1,55 +1,49 @@
-
 import "./Vision.css";
 import team from "../../team.png";
 import project from "../../project.png";
 
-function Vision(){
+function Vision() {
+  return (
+    <div className="vision-component">
+      <h1 className="vision-heading">What suits your vision? </h1>
+      <div className="vision-container">
+        <div className="vision-row">
+          <div className="team">
+            <img className="team-img" src={team} alt="Build Team" /> <br />
+            <div className="team-content">
+              <h1>Build Your Team</h1>
+              <p>
+                Build a dream team to work exclusively on your project,
+                well-aligned with your timeline and goals.
+              </p>
+            </div>
+            <button className="team-button">
+              Build my team <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          </div>
 
-    return(
-
-        <div className="vision-component">
-
-            <h1 className="vision-heading">What suits your vision? </h1>
-            <div className="container">
-                <div className="row vision-row" >
-                    <div className="team col-md-6 ">
-
-                        <img className="team-img" src= {team} alt="Build Team"/> <br />
-                        <div className="team-content">
-                            <h1>Build Your Team</h1>
-                            <p>Build a dream team to work exclusively on your project, well-aligned with your timeline and goals.</p>
-                        </div>
-
-                        <button className="team-button">
-                            Build my team  <i class="fa-solid fa-arrow-right"></i>
-                        </button>
-
-                    </div>
-                   
-
-                    <div className="project col-md-6">
-
-                        <img className="project-img"  src= {project} alt="Fixed price project"/> <br />
-                        <div className="project-content">
-
-                            <h1>Fixed Price Projects</h1>
-                            <p>Reduce your time-to-market with our fully-managed and cost-efficent project delivery.</p>
-                        </div>
-
-                        <button className="project-button">
-                            Develop my project <i class="fa-solid fa-arrow-right"></i>
-                        </button>
-
-                    </div>
-                    
-                </div>
-                </div>
- 
-
+          <div className="vision-project">
+            <img
+              className="vision-project-img"
+              src={project}
+              alt="Fixed price project"
+            />
+            <br />
+            <div className="project-content">
+              <h1>Fixed Price Projects</h1>
+              <p>
+                Reduce your time-to-market with our fully-managed and
+                cost-efficent project delivery.
+              </p>
+            </div>
+            <button className="project-button">
+              Develop my project <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          </div>
         </div>
-
-    );
-
+      </div>
+    </div>
+  );
 }
 
 export default Vision;
