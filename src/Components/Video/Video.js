0cde1b6043b16ea1ae546dad682760cf86c48959
Sama807 <samarafaqat091@gmail.com>
2023-12-09 11:44:13 +0500

    Video Component Added

---
+++ src/Components/Video/Video.js
@@ -0,0 +1,21 @@
+
+import "./Video.css";
+
+function Video(){
+
+    return(
+
+       <div className="video-head">
+         
+          <h1 className="heading-video">In our client's words</h1> <br /> <br />
+
+            <iframe className="video" width="1300" height="550" src="https://www.youtube.com/embed/zk_R_WUeoNQ" frameborder="0" allowfullscreen></iframe>
+
+       </div>
+
+
+    );
+
+
+}
+export default Video;
