68039c6f0a6303ac8ae6b6dfe068f46389b481a4
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-12-11 02:41:23 +0500

    adjusted the positioning of items in review and render review section dynamically using arrays and props

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,4 +1,5 @@
 import "./Reviews.css";
+import reviewArray from "./data.js";
 import client1 from "../../client1.jpg";
 import client2 from "../../client2.jpg";
 import client3 from "../../client3.jpg";
@@ -9,20 +10,24 @@ import client7 from "../../client7.jpg";
 import client8 from "../../client8.jpg";
 
 function ClientReview(props) {
-  alert("Hi");
   return (
     <div className="review-div col-md-6 ">
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
+      <div className="review-stars">
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+      </div>
 
       <p className="client-details">{props.detail}</p>
-
-      <img className="client-review" src={client1} alt="client-image" />
-      <p className="client-name">{props.name}</p>
-      <p className="client-info">{props.info}</p>
+      <div className="review-div-sub">
+        <img className="client-review-img" src={props.img} alt="client-image" />
+        <div className="review-div-sub-1">
+          <p className="client-name">{props.name}</p>
+          <p className="client-info">{props.info}</p>
+        </div>
+      </div>
     </div>
   );
 }
@@ -33,34 +38,29 @@ function Reviews() {
       <h1 className="reviews-heading">Don't take our words for it</h1>
 
       <div className="container r">
-        <div className="row ">
-          <ClientReview
-            name="Mara Nikolic"
-            info="Founder, Mara99"
-            detail="TxLabz provides great design and has a great sense of user experience and
-      aesthetics. They deliver on time and is an overall great communicator. I
-      highly recommend working with them."
-          />
-          {/* div2 */}
-          <div className="review-div col-md-6 ">
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-
-            <p className="client-details">
-              Amazing developers with an eye for aesthetics. We had pretty
-              vigorous requests and they hit every single one. They were patient
-              and highly skilled and ambitious.
-            </p>
-
-            <img className="client2" src={client2} alt="client2" />
-            <br />
-            <p className="client-name">John D Saunders</p>
-            <p className="client-info">Co-founder, 5fourdigital</p>
-          </div>
-        </div>
+        {Array.isArray(reviewArray) &&
+          reviewArray.map(
+            (review, index) =>
+              index + 1 < reviewArray.length &&
+              index % 2 === 0 && (
+                <div className="row" key={`group-${Math.floor(index / 2)}`}>
+                  <ClientReview
+                    name={review.name}
+                    info={review.info}
+                    img={review.img}
+                    detail={review.detail}
+                  />
+                  {index + 1 < reviewArray.length && (
+                    <ClientReview
+                      name={reviewArray[index + 1].name}
+                      info={reviewArray[index + 1].info}
+                      img={reviewArray[index + 1].img}
+                      detail={reviewArray[index].detail}
+                    />
+                  )}
+                </div>
+              )
+          )}
       </div>
     </div>
   );
